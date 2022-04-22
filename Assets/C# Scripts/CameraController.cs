c555decdeddfcc5f7eb58adf256a754f4143275b
CoWilll <hondpawns22@gmail.com>
2022-04-21 20:41:15 -0600

    HealthStuff
    Mid way done.

--- Assets/C# Scripts/CameraController.cs
+++ Assets/C# Scripts/CameraController.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask groundLayer, wallLayer;
     [SerializeField] private Transform player;
-    private BoxCollider2D boxCollider;
     [SerializeField] private BoxCollider2D playerCollider;
+    private bool jumpNotFinished = false;
+    
 
     private void Awake()
     {
@@ -16,15 +17,63 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.C))
+
+        /*if (Input.GetKey(KeyCode.Space))
+            jumpNotFinished = true;
+
+        if (IsGrounded())
+            jumpNotFinished = false;*/
+
+        if (Input.GetKey(KeyCode.W))
             transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
         else
             transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+
+
+        //option #2
+        //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        //If your not on the ground
+        /*if (!IsGrounded())
+        {
+            //actively jumping
+            if (OnWall())
+            {
+                transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+
+            } //Not actively jumping
+            else if (jumpNotFinished)
+            {
+                transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            } 
+            else if (Input.GetKey(KeyCode.Space))
+            {
+                
+                transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            }
+        
+        } else
+        {
+            transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        }*/
+
+
+
     }
 
-    private bool isGrounded()
+    private bool IsGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        //Note*: if the player is standing on the ground the collider will NOT equal null
+        return raycastHit.collider != null;
+    }
+
+    private bool OnWall()
+    {
+        Vector2 direction = new Vector2(player.localScale.x, 0);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, direction, 0.1f, wallLayer);
+
         //Note*: if the player is standing on the ground the collider will NOT equal null
         return raycastHit.collider != null;
     }
---
+++ Assets/C# Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, startingHealth);
+
+        //player alive
+        if (currentHealth > 0)
+        {
+
+        } 
+        //player dead
+        else
+        {
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
